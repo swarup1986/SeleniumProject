294b7be4948ce07c14592c5218802a61e9700b37
STantub <STantub@APINDLHCSX89H2.ra-int.com>
2020-09-24 00:47:21 +0530

    Added valid mess

--- Internship/src/main/java/validLogNavigation/ConnectWithUsFlow.java
+++ Internship/src/main/java/validLogNavigation/ConnectWithUsFlow.java
@@ -1,22 +1,27 @@
 package validLogNavigation;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class ConnectWithUsFlow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Swarup\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.validlog.com/contact-us/");
 		driver.manage().window().maximize();
-		driver.findElement(By.id("form-field-name")).sendKeys("a");
-		driver.findElement(By.id("form-field-phone")).sendKeys("3");
-		driver.findElement(By.id("form-field-email")).sendKeys("ee");
-		driver.findElement(By.id("form-field-message")).sendKeys("ddd");
-		driver.findElement(By.xpath("//span[contains(text(),'Send')]")).click();	
+		driver.findElement(By.id("form-field-name")).sendKeys("test1_swa");
+		driver.findElement(By.id("form-field-phone")).sendKeys("7744");
+		driver.findElement(By.id("form-field-email")).sendKeys("test_swa@swa.com");
+		driver.findElement(By.id("form-field-message")).sendKeys("test1_swa");
+		driver.findElement(By.xpath("//span[contains(text(),'Send')]")).click();
+		Thread.sleep(4000);
+		driver.quit();
 
 	}
 
